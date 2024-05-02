376fa1b9b44f3db9aa36e89a72ca3b185f190620
sfrunza <frunza.sergiu3@gmail.com>
2024-05-02 03:05:49 -0400

    socket

--- client/src/api/WebSocketConnection.ts
+++ client/src/api/WebSocketConnection.ts
@@ -1,5 +1,5 @@
 import ActionCable from 'actioncable';
 
-const cable = ActionCable.createConsumer('ws://localhost:3001/cable');
+const cable = ActionCable.createConsumer(process.env.REACT_APP_WS_URL || 'ws://localhost:3001/cable');
 
 export default cable;
